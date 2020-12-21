5ab09ae571ab558cc67b7ea74151ab456b56cdee
abadire <panvan97@gmail.com>
2020-12-21 21:40:32 +0300

    init: create app, initial setup, add reset

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  {{ user.username }}
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      user: {
+        username: 'van',
+      },
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: violet;
+  width: 100%;
+  height: 100vh;
+}
+</style>
